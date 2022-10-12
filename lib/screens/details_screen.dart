8a9a70389fa930d92d0f9c54fdf6271da986722e
Luis Fernando Montoya <luisfdomontoya@outlook.com>
2022-10-12 17:52:11 -0500

    Slivers - SliverAppBar

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -5,9 +5,48 @@ class DetailsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('DetailsScreen'),
+    // ModalRoute.of(context)?.settings.arguments: me permite recibir el
+    // argumento q le envié por el Navigator y se lo asigno a movie. Es
+    // importante mencionar q arguments devuelve un objeto y para poder
+    // usar este valor y asignarlo a movie (que es un string) debo
+    // convertirlo a string usando el método toString
+    final String movie =
+        ModalRoute.of(context)?.settings.arguments.toString() ?? 'no-movie';
+
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [_CustomAppBar()],
+      ),
+    );
+  }
+}
+
+class _CustomAppBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SliverAppBar(
+      backgroundColor: Colors.indigo,
+      expandedHeight: 200,
+      floating: false,
+      pinned: true,
+      flexibleSpace: FlexibleSpaceBar(
+        centerTitle: true,
+        titlePadding: const EdgeInsets.all(0),
+        title: Container(
+          width: double.infinity,
+          alignment: Alignment.bottomCenter,
+          color: Colors.black12,
+          child: const Text(
+            'movie.title',
+            style: TextStyle(fontSize: 16),
+          ),
+        ),
+        background: const FadeInImage(
+          placeholder: AssetImage('assets/loading.gif'),
+          image: NetworkImage('https://via.placeholder.com/500x300'),
+          //permite que la imagen se expanda sin deformarla
+          fit: BoxFit.cover,
+        ),
       ),
     );
   }

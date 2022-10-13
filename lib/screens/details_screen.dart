63f29a164c083a02a4a7bed8e9382ab1c8e83730
Luis Fernando Montoya <luisfdomontoya@outlook.com>
2022-10-13 09:30:16 -0500

    details_screen -- movie title

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -19,7 +19,7 @@ class DetailsScreen extends StatelessWidget {
           _CustomAppBar(),
           SliverList(
             delegate: SliverChildListDelegate([
-              const Text('hello worl'),
+              _PosterAndTitle(),
             ]),
           )
         ],
@@ -55,6 +55,55 @@ class _CustomAppBar extends StatelessWidget {
           //permite que la imagen se expanda sin deformarla
           fit: BoxFit.cover,
         ),
+      ),
+    );
+  }
+}
+
+class _PosterAndTitle extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final TextTheme textTheme = Theme.of(context).textTheme;
+    return Container(
+      margin: const EdgeInsets.only(top: 20),
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Row(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: const FadeInImage(
+              placeholder: AssetImage('assets/no-image.jpg'),
+              image: NetworkImage('https://via.placeholder.com/200x300'),
+              // width: 130,
+              height: 150,
+              // fit: BoxFit.cover,
+            ),
+          ),
+          const SizedBox(width: 20),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'movie.title',
+                style: textTheme.headline5,
+                overflow: TextOverflow.ellipsis,
+                maxLines: 2,
+              ),
+              Text(
+                'movie.originalTitle',
+                style: textTheme.subtitle1,
+                overflow: TextOverflow.ellipsis,
+              ),
+              Row(
+                children: [
+                  const Icon(Icons.star_outline, size: 15, color: Colors.grey),
+                  const SizedBox(width: 5),
+                  Text('movie.voteAverage', style: textTheme.caption)
+                ],
+              )
+            ],
+          )
+        ],
       ),
     );
   }

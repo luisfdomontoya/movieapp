ca648ba034285608b06030b6e1679f796d531fd2
Luis Fernando Montoya <luisfdomontoya@outlook.com>
2022-10-13 15:54:28 -0500

    provider -- part I

---
+++ lib/providers/movies_provider.dart
@@ -0,0 +1,11 @@
+class MoviesProvider {
+  MoviesProvider() {
+    print('MoviesProvider initializing');
+
+    this.getOnDisplayMovies();
+  }
+
+  getOnDisplayMovies() async {
+    print('getOnDisplayMovies');
+  }
+}

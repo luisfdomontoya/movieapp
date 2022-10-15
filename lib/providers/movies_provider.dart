c2d63ce0f25ddd85213faffd4dfb17539a147083
Luis Fernando Montoya <luisfdomontoya@outlook.com>
2022-10-15 11:45:11 -0500

    provider -- part III

--- lib/providers/movies_provider.dart
+++ lib/providers/movies_provider.dart
@@ -1,9 +1,19 @@
+//Para poder usar json en nuestra petición
+//debemos importar este paquete, aunque lo puedo autoimportar
+//en el momento que uso json:
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 //Importante: nuestra clase provider (MoviesProvier) debe
 //extender de la lcase ChangeNotifier para que sea realemente
 //un provider:
 class MoviesProvider extends ChangeNotifier {
+  String _apiKey = '73fcf29ebbd77050307a4cc197f2dfe7';
+  String _baseUrl = 'api.themoviedb.org';
+  String _lenguage = 'en-US';
+
   MoviesProvider() {
     print('MoviesProvider initializing');
 
@@ -14,6 +24,28 @@ class MoviesProvider extends ChangeNotifier {
   }
 
   getOnDisplayMovies() async {
-    print('getOnDisplayMovies');
+    var url = Uri.https(_baseUrl, '/3/movie/now_playing', {
+      'api_key': _apiKey,
+      'language': _lenguage,
+      'page': '1',
+    });
+
+    //1. Esperamos la respuesta de la petición http get
+    final response = await http.get(url); //esto devuelve un json
+
+    //2. Luego de obtener la respuesta necesitamos convertirla a algo
+    //para poder usar esa información en nuestra aplicación. Una
+    //forma de hacerlo es convertir la respuesta en un mapa. Para que
+    //json.decode sepa que necesita convertir a un mapa le debo indicar
+    //en el lado izquiero el tipo de dato que es decodedData, así:
+    final Map<String, dynamic> decodedData = json.decode(response.body);
+
+    //3. El paso siguiente es mapear el mapa decodedData a una clase.
+    //Este mapeo se puede hacer manual (lo cual tomaría mucho tiempo) o
+    //podemos usar el sitio web quicktype.io. En este caso usan el sitio
+    //web. Lo que hace quicktype es crear una clase que se adapte
+    //exactamente al json que le pasemos.
+
+    print(decodedData);
   }
 }

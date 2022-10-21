14c208afe796137411da7a3fb639dc3095f4fac1
Luis Fernando Montoya <luisfdomontoya@outlook.com>
2022-10-21 13:41:47 -0500

    Search button II

--- lib/search/search_delegate.dart
+++ lib/search/search_delegate.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:movieapp/models/models.dart';
+import 'package:movieapp/providers/movies_provider.dart';
+import 'package:provider/provider.dart';
 
 class MovieSearchDelegate extends SearchDelegate {
   //Este método sobre-escribe el label que viene en el
-  //input searh:
+  //input searh, por lo tanto, yo le puedo mandar cualquier
+  //string. En este caso le estoy mandando 'Searching
+  //Movies...':
   @override
   // TODO: implement searchFieldLabel
   String? get searchFieldLabel => 'Searching Movies...';
@@ -21,6 +26,14 @@ class MovieSearchDelegate extends SearchDelegate {
   Widget? buildLeading(BuildContext context) {
     return (IconButton(
       onPressed: () {
+        //el método close viene gracias al SearchDelegate. El
+        //dato importante aquí es el argumento que recibe close.
+        //Por un lado recibe el context y por otra lado recibe
+        //otro argumento que se convertirá en el valor de
+        //retorno de la llamada a showSearch (que lo usamos en
+        //el HomeScreen). En este caso le pasamos null porque no
+        //queremos que haga nada, así que simplemente me devuelve
+        //a la pantalla de HomeScreen:
         close(context, null);
       },
       icon: const Icon(Icons.arrow_back),
@@ -29,23 +42,40 @@ class MovieSearchDelegate extends SearchDelegate {
 
   @override
   Widget buildResults(BuildContext context) {
-    return Text('buildResults');
+    return const Text('buildResults');
+  }
+
+  Widget _emptyContainer() {
+    return Container(
+      child: const Center(
+        child: Icon(
+          Icons.movie_creation_outlined,
+          color: Colors.black38,
+          size: 140,
+        ),
+      ),
+    );
   }
 
   @override
   Widget buildSuggestions(BuildContext context) {
+    //tenga en cuenta que la palabra reservada 'query' viene
+    //del SearchDelegate, por eso la puedo usar aquí. Y lo
+    //que almacena query es el texto que yo pongo en el input
+    //del search:
     if (query.isEmpty) {
-      return Container(
-        child: const Center(
-          child: Icon(
-            Icons.movie_creation_outlined,
-            color: Colors.black38,
-            size: 130,
-          ),
-        ),
-      );
+      return _emptyContainer();
     }
 
-    return Container();
+    final moviesProvider = Provider.of<MoviesProvider>(context, listen: false);
+
+    return FutureBuilder(
+      future: moviesProvider.searchMovies(query),
+      builder: (context, AsyncSnapshot<List<Movie>> snapshot) {
+        if (!snapshot.hasData) return _emptyContainer();
+
+        return;
+      },
+    );
   }
 }
